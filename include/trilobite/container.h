74bff61d70fef566f89c1fbe5a733e6275767411
Michael Gene Brockus <michaelbrockus@gmail.com>
2023-03-01 19:35:41 -0600

    commit main header

---
+++ include/trilobite/container.h
@@ -0,0 +1,34 @@
+/*
+   under:   trilobite stdlib
+   author:  Michael Gene Ridge Brockus
+   gmail:   <michaelbrockus@gmail.com>
+   website: <https://trilobite.code.blog>
+*/
+#ifndef TRILOBITE_CONTAINER_H
+#define TRILOBITE_CONTAINER_H
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+//
+//   PUBLIC APPLICATION INTERFACE
+// --------------------------------
+//
+// Published library with provided public application interface for
+// use in the users application. Please note that we are internationally
+// targeting both c11 and c23 standards.
+//   
+#include "queue.h"
+#include "dqueue.h"
+#include "stack.h"
+#include "stack.h"
+#include "slist.h"
+#include "dlist.h"
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
